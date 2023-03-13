89b0089493515a0f44c364491706a5490e0a3703
Dina Nevhod <Dina_Nevhod@epam.com>
2023-03-13 21:16:07 +0100

    Task12.6

---
+++ 003_MVP/MVP/Presenter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+// Presenter
+
+namespace MVP
+{
+    class Presenter
+    {
+        Model model = null;
+        MainWindow view = null;
+
+        public Presenter(MainWindow mainWindow)
+        {
+            this.model = new Model();
+            this.view = mainWindow;
+            this.view.myEvent += new EventHandler(mainWindow_myEvent);
+            this.view.myEvent += new EventHandler(Add_myEvent);
+            this.view.myEvent += new EventHandler(Remove_myEvent);
+        }
+
+        void mainWindow_myEvent(object sender, System.EventArgs e)
+        {
+            var data = this.view.textBox1.Text;
+
+            string variable = model.Logic(data);
+
+            this.view.textBox1.Text = variable;
+        }
+        void Add_myEvent(object sender, System.EventArgs e)
+        {
+            var data = this.view.textBox1.Text;
+
+            string variable = model.Logic(data);
+
+            this.view.textBox1.Text += "Add";
+        }
+        void Remove_myEvent(object sender, System.EventArgs e)
+        {
+            this.view.myEvent -= new EventHandler(mainWindow_myEvent);
+            var data = this.view.textBox1.Text;
+            string variable = model.Logic(data);
+
+            this.view.textBox1.Text = data;
+        }
+    }
+}
